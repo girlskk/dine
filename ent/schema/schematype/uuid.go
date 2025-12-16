546245fd66bc7f4c2472cf9df442b78cf30b86a7
jerry.mei <king.mj1022@gmail.com>
2025-12-16 15:40:16 +0800

    feat: 加入ent代码

---
+++ ent/schema/schematype/uuid.go
@@ -0,0 +1,20 @@
+package schematype
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+	"github.com/google/uuid"
+)
+
+type UUIDMixin struct {
+	mixin.Schema
+}
+
+func (UUIDMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New).
+			Comment("UUID as primary key"),
+	}
+}
