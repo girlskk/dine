81811cc3b0c06e7b5d6ba745f4f486da4b91708b
liran <rli187617@gmail.com>
2026-01-08 15:26:48 +0800

    feat:商品oss上传

--- api/admin/types/oss.go
+++ api/admin/types/oss.go
@@ -7,9 +7,9 @@ import (
 
 // OssTokenReq 用于获取OSS上传凭证的请求参数
 type OssTokenReq struct {
-	Scene       domain.ObjectStorageScene `json:"scene" binding:"required,oneof=store merchant"` // 业务场景，store：门店相关 merchant: 商户相关
-	Filename    string                    `json:"filename" binding:"required"`                   // 文件名
-	ForDownload bool                      `json:"for_download"`                                  // 是否用于下载
+	Scene       domain.ObjectStorageScene `json:"scene" binding:"required,oneof=store merchant product"` // 业务场景，store：门店相关 merchant: 商户相关 product: 商品相关
+	Filename    string                    `json:"filename" binding:"required"`                           // 文件名
+	ForDownload bool                      `json:"for_download"`                                          // 是否用于下载
 }
 
 // OssTokenResp 用于获取OSS上传凭证的响应参数
